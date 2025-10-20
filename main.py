0228339ad8be5db3eb882e948d72c8d5b98cd832
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2025-10-21 00:10:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+#from bot import Bot
+
+import asyncio
+
+# Check if an event loop is already set on the current thread
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    # If no loop is set, create a new one and set it
+    asyncio.set_event_loop(asyncio.new_event_loop())
+
+# The rest of your imports can follow
 from bot import Bot
+# ...
+
 
 if __name__ == '__main__':
     Bot().run()
